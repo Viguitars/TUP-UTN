5571fcb7bc7ca346f120f4eb55f34d358c34d474
Víctor Gomenza <viguitars738@gmail.com>
2023-08-31 23:06:58 -0300

    Update ejercicio_for-while.py

--- Programacion I/ejercicio_for-while.py
+++ Programacion I/ejercicio_for-while.py
@@ -2,6 +2,7 @@
 
 # EJERCICIOS EN CLASE FOR - WHILE
 
+
 # 1- Un grupo de amigos decide organizar un juego de estrategia, para lo cual
 # forman dos equipos de 6 integrantes cada uno, donde un integrante de cada
 # equipo es el “jefe” y los otros 5 son sus “oficiales”.
@@ -26,8 +27,74 @@
 # la “z”: (índice de la letra a correr+corrimiento)%27
 # Sólo se encriptarán las letras de los mensajes, dejando al resto de
 # caracteres sin modificación.
+def encrypt_message(message, shift):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    encrypted_message = ""
+
+    for char in message:
+        if char.isalpha():
+            is_uppercase = char.isupper()
+            index = alphabet.index(char.upper())
+            encrypted_index = (index + shift) % len(alphabet)
+            encrypted_char = alphabet[encrypted_index]
+
+            if not is_uppercase:
+                encrypted_char = encrypted_char.lower()
+
+            encrypted_message += encrypted_char
+        else:
+            encrypted_message += char
+
+    return encrypted_message
+
+
+shift = int(input("Ingrese el corrimiento para la encriptación: "))
+boss_message = input("Ingrese el mensaje del jefe: ")
+officers_messages = []
+
+for i in range(5):
+    message = input(f"Ingrese el mensaje del oficial {i + 1}: ")
+    officers_messages.append(message)
+
+boss_encrypted = encrypt_message(boss_message, shift)
+officers_encrypted = [encrypt_message(message, shift) for message in officers_messages]
+
+print("\nMensaje encriptado del jefe:", boss_encrypted)
+for i, encrypted_message in enumerate(officers_encrypted, start=1):
+    print(f"Mensaje encriptado del oficial {i}:", encrypted_message)
+
 
 # 2- Crear un programa que solicite el ingreso de números enteros positivos,
 # hasta que el usuario ingrese el 0. Por cada número, informar cuántos dígitos
 # pares y cuántos impares tiene. Al finalizar, informar la cantidad de dígitos
 # pares y de dígitos impares leídos en total.
+def count_even_odd_digits(number):
+    even_digits = 0
+    odd_digits = 0
+
+    while number > 0:
+        digit = number % 10
+        if digit % 2 == 0:
+            even_digits += 1
+        else:
+            odd_digits += 1
+        number //= 10
+
+    return even_digits, odd_digits
+
+
+total_even_digits = 0
+total_odd_digits = 0
+
+number = int(input("Ingrese un número (0 para salir): "))
+
+while number != 0:
+    even_digits, odd_digits = count_even_odd_digits(number)
+    total_even_digits += even_digits
+    total_odd_digits += odd_digits
+    print(f"Dígitos pares: {even_digits}, Dígitos impares: {odd_digits}")
+    number = int(input("Ingrese otro número (0 para salir): "))
+
+print(
+    f"Total dígitos pares: {total_even_digits}, Total dígitos impares: {total_odd_digits}"
+)
