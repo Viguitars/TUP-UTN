c8f6881aa6405ca58b6132e4f55161bed83853c2
Víctor Gomenza <viguitars738@gmail.com>
2023-09-03 20:32:39 -0300

    Create formulario.js

---
+++ Sistemas de Procesamiento de Datos/TP Formularios/formulario.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	const data = Object.fromEntries(new FormData(e.target));
+	console.log(JSON.stringify(data));
+});
