d11fa367a1ce6ffbf01b638fd27241a7fefccac1
Víctor Gomenza <viguitars738@gmail.com>
2023-09-27 20:07:29 -0300

    Update 13_ejercicio_ahorcado.py

--- Programacion I/13_ejercicio_ahorcado.py
+++ Programacion I/13_ejercicio_ahorcado.py
@@ -1,3 +1,5 @@
+import funciones_ahorcado as fa
+
 """ Crea un juego interactivo del ahorcado en Python. El juego debe seleccionar 
 una palabra al azar de una lista de palabras predefinidas y permitir que el 
 jugador adivine la palabra letra por letra. El jugador tiene un número limitado 
@@ -28,3 +30,42 @@ Consideraciones adicionales:
 * Puedes utilizar funciones para organizar tu código de manera efectiva.
 * Agrega comentarios para explicar el funcionamiento de tu código. """
 
+
+# Este código implementa un juego del ahorcado interactivo en Python
+
+# Inicializar variables
+secret_word = fa.select_word()
+remaining_attempts = 6
+board = fa.initialize_board(secret_word)
+
+print("¡Bienvenido al Juego del Ahorcado!")
+
+# Mostrar la longitud de la palabra con "_"
+fa.display_board(board)
+
+# Loop principal
+while remaining_attempts > 0:
+    letter = input("Adivina una letra: ").lower()
+    # Verifica que sea una letra
+    if len(letter) != 1 or not letter.isalpha():
+        print("Ingresa una letra válida.")
+        continue
+    # Verifica que la letra no se haya introducido antes
+    if letter in board:
+        print("Ya has adivinado esa letra.")
+        continue
+    # Si la letra esta en la palabra elegida
+    if fa.guess_letter(secret_word, board, letter):
+        print("¡Adivinaste una letra correctamente!")
+    else:
+        remaining_attempts -= 1
+        print(f"Letra incorrecta. Te quedan {remaining_attempts} intentos.")
+
+    fa.display_board(board)
+    # Si en la palabra no hay "_", se finaliza el programa
+    if "_" not in board:
+        print("¡Felicidades! Has adivinado la palabra correctamente.")
+        break
+# Si se superan los intentos permitidos, se muestra la solución
+if "_" in board:
+    print(f"¡Agotaste tus intentos! La palabra correcta era '{secret_word}'.")
