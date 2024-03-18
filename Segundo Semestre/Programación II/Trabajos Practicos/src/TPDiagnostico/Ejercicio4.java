5ef1a2b8097629c0087c3a20edbc8ab7faa69c89
Víctor Gomenza <viguitars738@gmail.com>
2024-03-18 01:15:35 -0300

    Create Ejercicio4.java

---
+++ Segundo Semestre/Programación II/Trabajos Practicos/src/TPDiagnostico/Ejercicio4.java
@@ -0,0 +1,15 @@
+package TPDiagnostico;
+
+import java.util.Scanner;
+
+/**
+ * Pide un número por teclado e indica si es un número primo o no.
+ */
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Prompt to user for the first number
+        System.out.println("Enter a int number:");
+        int num = sc.nextInt();
+    }
+}
