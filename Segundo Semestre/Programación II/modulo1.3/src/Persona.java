cdef297339ed866051d35a76d43806583b357f9f
Víctor Gomenza <viguitars738@gmail.com>
2024-03-18 01:32:09 -0300

    Create Persona.java

---
+++ Segundo Semestre/Programación II/modulo1.3/src/Persona.java
@@ -0,0 +1,9 @@
+public class Persona extends Animal {
+    // Sobre escritura de miembros
+    @Override
+    public void comer() {
+
+        System.out.println("Estoy comiendo sentado y con cubiertos.");
+    }
+
+}
