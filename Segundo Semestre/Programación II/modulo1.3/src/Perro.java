97f7ae1b97a971850bc8cb7cf01e9855959357e0
Víctor Gomenza <viguitars738@gmail.com>
2024-03-18 01:32:13 -0300

    Create Perro.java

---
+++ Segundo Semestre/Programación II/modulo1.3/src/Perro.java
@@ -0,0 +1,8 @@
+public class Perro extends Animal {
+    // Sobre escritura de miembros
+    @Override
+    public void comer() {
+        System.out.println("Estoy comiendo con mi plato en el suelo.");
+    }
+
+}
