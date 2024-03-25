cb24bd3816d4da8fa29cb891a629fc09ab2939fd
Víctor Gomenza <viguitars738@gmail.com>
2024-03-24 22:17:40 -0300

    Create Main.java

---
+++ Segundo Semestre/Laboratorio de Computación II/UML/src/Clase01_1403/Main.java
@@ -0,0 +1,8 @@
+package Clase01_1403;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Victor", 39, 71, 178);
+        Persona persona2 = new Persona("Alejandra", 38, 55, 164);
+    }
+}
