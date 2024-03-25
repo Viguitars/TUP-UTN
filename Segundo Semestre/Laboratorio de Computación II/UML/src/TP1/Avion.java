a8617a91b8792d3ec73e5cfc9065a36eb0bffd59
Víctor Gomenza <viguitars738@gmail.com>
2024-03-24 22:17:53 -0300

    Create Avion.java

---
+++ Segundo Semestre/Laboratorio de Computación II/UML/src/TP1/Avion.java
@@ -0,0 +1,53 @@
+package TP1;
+
+public class Avion {
+    private String matricula;
+    private int capacidad;
+    private String modelo;
+
+    public Avion() {
+
+    }
+
+    public Avion(String matricula, int capacidad, String modelo) {
+        this.matricula = matricula;
+        this.capacidad = capacidad;
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void despegar() {
+        System.out.println("El avión ha despegado.");
+
+    }
+
+    public void aterrizar() {
+        System.out.println("El avión ha aterrizado.");
+
+    }
+
+
+}
