de2f57b292f934211bc6b798876724bd1bc9d128
Víctor Gomenza <viguitars738@gmail.com>
2024-03-24 22:19:11 -0300

    Create Lobo.java

---
+++ Segundo Semestre/Programación II/Modulos/modulo01/src/clase03_2103/Ejercicio2/Lobo.java
@@ -0,0 +1,10 @@
+package clase03_2103.Ejercicio2;
+
+public class Lobo extends Canino{
+    public Lobo(String foto, String comida, String localizacion, String tamanio) {
+        super(foto, comida, localizacion, tamanio);
+    }
+    public void comer(){}
+
+    public void hacerRuido(){}
+}
