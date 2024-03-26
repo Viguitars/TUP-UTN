cb1e398543ff312d14bd876c2686cad5c85be6b6
Víctor Gomenza <viguitars738@gmail.com>
2024-03-25 22:35:43 -0300

    Create Main.java

---
+++ Segundo Semestre/Programación II/Herencia.1903/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        SavingsAccount savingsAccount = new SavingsAccount(12000, 3);
+
+        savingsAccount.deposit(2000);
+
+        savingsAccount.withdraw(500);
+
+        savingsAccount.monthlyStatement();
+
+        savingsAccount.print();
+    }
+}
