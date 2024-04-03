ee10e5c0b66467e7a4e9bf45d8d1cd4ea9ab70c4
Víctor Gomenza <viguitars738@gmail.com>
2024-04-03 20:46:05 -0300

    Create Main.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase04_0304/Ejercicio2/Main.java
@@ -0,0 +1,19 @@
+package clases.clase04_0304.Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+//            Se crea un array de cinco elementos
+            int[] numeros = new int[5];
+
+//            Se intenta acceder al elemento seis del array (indice fuera de bounds)
+            int numero = numeros[6];
+
+            System.out.println("El número es: " + numero);
+        } catch (ArrayIndexOutOfBoundsException e) {
+//            Se captura la excepción ArrayIndexOutOfBoundsException
+            System.out.println("Error: " + e.getMessage());
+            System.out.println("Fuera de parámetro.");
+        }
+    }
+}
