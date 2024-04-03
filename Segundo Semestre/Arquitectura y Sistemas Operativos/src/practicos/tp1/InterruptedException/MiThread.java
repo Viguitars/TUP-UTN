a3f9078b1174ff5382b43cfbe5fe28fa91fea294
Víctor Gomenza <viguitars738@gmail.com>
2024-04-02 22:45:17 -0300

    Create MiThread.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/InterruptedException/MiThread.java
@@ -0,0 +1,14 @@
+package practicos.tp1.InterruptedException;
+
+public class MiThread extends Thread {
+    public void run() {
+        while (true) {
+            System.out.println("Ejecuto");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Termino en sleep");
+            }
+        }
+    }
+}
