285621a28c37ecbb6c2d47e9f817fc6fd5c63c2d
Víctor Gomenza <viguitars738@gmail.com>
2024-04-03 20:45:29 -0300

    Create EdadInvalidaException.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase04_0304/Ejercicio1/EdadInvalidaException.java
@@ -0,0 +1,7 @@
+package clases.clase04_0304.Ejercicio1;
+
+public class EdadInvalidaException extends Exception {
+    public EdadInvalidaException(String message) {
+        super(message);
+    }
+}
