2a879a0a2c1db5a1424870b3df6fe8382066d49d
Víctor Gomenza <viguitars738@gmail.com>
2024-04-02 21:58:53 -0300

    Create Ejemplo_join.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/join/Ejemplo_join.java
@@ -0,0 +1,26 @@
+package practicos.tp1.join;
+
+public class Ejemplo_join {
+    public static void main(String[] args) {
+        Obrera agente = new Obrera();
+        agente.start();
+        // Hace algo durante el cálculo.
+        try { //Espera a que agente termine
+            agente.join();
+        } catch (InterruptedException e) {
+        }
+        ;
+        // Utiliza el resultado.
+        System.out.println(agente.getResultado());
+    }
+}
+
+/**
+ * El código presenta una clase Obrera que extiende la clase Thread. Esta clase
+ * realiza un cálculo largo en el método Calcula(), que simula tomar 10 segundos
+ * en completarse. Durante la ejecución de este cálculo, el hilo principal
+ * continúa con sus tareas.
+ * El método getResultado() devuelve el resultado del cálculo, que inicialmente
+ * está marcado como "No calculado". Una vez que el cálculo finaliza, este
+ * resultado se actualiza a "Ya calculado".
+ */
