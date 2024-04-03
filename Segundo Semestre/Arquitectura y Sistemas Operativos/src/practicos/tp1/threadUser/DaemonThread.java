ea6ec45803000db1659a810634f61e6f61cd3099
Víctor Gomenza <viguitars738@gmail.com>
2024-04-03 20:46:12 -0300

    Update DaemonThread.java

--- Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/threadUser/DaemonThread.java
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/threadUser/DaemonThread.java
@@ -28,3 +28,23 @@ public class DaemonThread implements Runnable {
         System.out.println("Termina main()");
     }
 }
+
+/**
+ * 1. La clase DaemonThread implementa la interfaz Runnable y sobrescribe el
+ * método run(). Dentro de este método, hay un bucle while infinito que duerme el
+ * hilo durante 500 milisegundos en cada iteración utilizando Thread.sleep(500).
+ * Después de despertar del sueño, se imprime "run() ha despertado". Este bucle
+ * se ejecuta continuamente hasta que el hilo es interrumpido.
+ * 2. En el método main(), se crea una instancia de DaemonThread y se envuelve
+ * en un nuevo hilo t. Antes de iniciar el hilo, se establece como un hilo
+ * demonio utilizando t.setDaemon(true). Los hilos demonio son hilos que se
+ * ejecutan en segundo plano y no impiden que el programa finalice si todos
+ * los hilos normales ya han terminado su ejecución.
+ * 3. El hilo t se inicia y comienza a ejecutar su método run() en segundo
+ * plano.
+ * 4. Después de iniciar el hilo, el hilo principal (el hilo que ejecuta el
+ * método main()) se duerme durante 2000 milisegundos utilizando Thread.sleep
+ * (2000).
+ * 5. Cuando el tiempo de espera del hilo principal termina, se imprime "Termina
+ * main()" y el programa finaliza su ejecución.
+ */
