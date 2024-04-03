26d508b760f5e59b8f8afe03e61e6a82007660fd
Víctor Gomenza <viguitars738@gmail.com>
2024-04-02 22:45:32 -0300

    Create DaemondThread.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/threadDaemond/DaemondThread.java
@@ -0,0 +1,30 @@
+package practicos.tp1.threadDaemond;
+
+public class DaemondThread implements Runnable {
+    public void run() {
+        System.out.println("Comienza run()");
+        try {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+                System.out.println("run() ha despertado");
+            }
+        } finally {
+            System.out.println("Termina run()");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Comienza main()");
+        Thread t = new Thread(new DaemondThread());
+        t.setDaemon(true);
+        t.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("Termina main()");
+    }
+}
