acc1eb0bf54be51e22a08e22b5df135378c08875
Víctor Gomenza <viguitars738@gmail.com>
2024-04-08 21:30:34 -0300

    Update Ejercicio1.R

--- Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio1.R
+++ Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio1.R
@@ -1,13 +1,39 @@
 # Datos
-carreras <- c("Medicina", "Derecho", "Ciencias", "Letras", "Inef", "Otras")
+carreras <-
+  c("Medicina", "Derecho", "Ciencias", "Letras", "Inef", "Otras")
 alumnos <- c(250, 176, 127, 314, 103, 30)
 
-# Crear un dataframe con los datos
-datos <- data.frame(Carrera = carreras, Alumnos = alumnos)
+# Frecuencia
+frecuencia <- alumnos
 
-# Mostrar el dataframe
+# Frecuencia relativa
+frecuencia_relativa <- alumnos / sum(alumnos)
+
+# Frecuencia acumulada
+frecuencia_acumulada <- cumsum(frecuencia)
+
+# Frecuencia acumulada relativa
+frecuencia_acumulada_relativa <- cumsum(frecuencia_relativa)
+
+# Crear un dataframe con los datos
+datos <-
+  data.frame(
+    Carrera = carreras,
+    Alumnos = alumnos,
+    f = frecuencia,
+    fr = frecuencia_relativa,
+    F = frecuencia_acumulada,
+    Fr = frecuencia_acumulada_relativa
+  )
+
+# Imprimir datos
 print(datos)
 
 # Crear un gráfico de barras
-barplot(alumnos, names.arg = carreras, xlab = "Carrera", ylab = "Número de Alumnos", col = "skyblue", main = "Carreras Elegidas por los Alumnos")
-
+barplot(
+  alumnos,
+  names.arg = carreras,
+  xlab = "Carreras",
+  ylab = "Número de alumnos",
+  main = "Carreras elegidas por los alumnos",
+)
