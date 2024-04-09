76fa9cd502a815b677eaa8c61edc76115c99822a
Víctor Gomenza <viguitars738@gmail.com>
2024-04-08 22:43:55 -0300

    Update Ejercicio2.R

--- Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio2.R
+++ Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio2.R
@@ -1,17 +1,89 @@
 # Datos
-longitud <- c(rep(1, 2), rep(2, 10), rep(3, 10), rep(4, 5), rep(5, 10), rep(6, 3), rep(7, 2), rep(8, 2))
+longitudes <- c(1, 2, 3, 4, 5, 6, 7, 8)
+num_niños <- c(2, 10, 10, 5, 10, 3, 2, 2)
 
-# Crear la tabla de frecuencias
-tabla_frecuencias <- table(longitud)
+# Frecuencia
+frecuencia <- num_niños
 
-# Convertir la tabla de frecuencias en un dataframe para facilitar su manipulación
-df_frecuencias <- as.data.frame(tabla_frecuencias)
+# Frecuencia relativa
+frecuencia_relativa <- num_niños / sum(num_niños)
 
-# Renombrar las columnas del dataframe
-names(df_frecuencias) <- c("Longitud", "Frecuencia")
+# Frecuencia acumulada
+frecuencia_acumulada <- cumsum(frecuencia)
 
-# Mostrar la tabla de frecuencias
-print(df_frecuencias)
+# Frecuencia acumulada relativa
+frecuencia_acumulada_relativa <- cumsum(frecuencia_relativa)
 
-# Crear un histograma
-hist(longitud, breaks = 8, xlab = "Longitud en metros", ylab = "Frecuencia", main = "Distribución de Longitudes de Caminar")
+# Crear un dataframe con los datos
+datos <- data.frame(
+  Longitud = longitudes,
+  Niños = num_niños,
+  f = frecuencia,
+  fr = round(frecuencia_relativa, 3),
+  F = frecuencia_acumulada,
+  Fr = round(frecuencia_acumulada_relativa, 3)
+)
+
+# Imprimir datos
+print(datos)
+
+# Crear el lienzo del gráfico
+par(mfrow = c(1, 2))  # Dividir la ventana gráfica en 1 fila y 2 columnas
+
+# Graficar barras de la cantidad de niños
+barplot(
+  num_niños,
+  names.arg = longitudes,
+  col = "skyblue",
+  xlab = "Número de metros",
+  ylab = "Número de niños",
+  main = "Cantidad de niños por longitud"
+)
+
+# Graficar las frecuencias y sus acumuladas
+plot(
+  1:length(longitudes),
+  frecuencia,
+  type = "h",
+  lwd = 2,
+  col = "blue",
+  ylim = c(0, max(frecuencia_acumulada) * 1.2),
+  xlab = "Número de metros",
+  ylab = "Frecuencia",
+  main = "Gráfico de frecuencia y frecuencias acumuladas",
+  xaxt = "n"
+)
+axis(side = 1,
+     at = 1:length(longitudes),
+     labels = longitudes)
+
+# Agregar la línea para la frecuencia acumulada
+lines(
+  1:length(longitudes),
+  frecuencia_acumulada,
+  type = "b",
+  pch = 16,
+  col = "red"
+)
+
+# Agregar los puntos para la frecuencia acumulada relativa
+points(
+  1:length(longitudes),
+  frecuencia_acumulada_relativa * max(frecuencia),
+  pch = 19,
+  col = "green"
+)
+
+# Agregar leyenda
+legend(
+  "topright",
+  legend = c(
+    "Frecuencia",
+    "Frecuencia acumulada",
+    "Frecuencia acumulada relativa"
+  ),
+  col = c("blue", "red", "green"),
+  pch = c(15, 16, 19),
+  lty = c(0, 1, 0)
+)
+
