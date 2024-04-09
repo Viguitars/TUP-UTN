99f30174fa99cba771b17cfe1c652a54107ab003
Víctor Gomenza <viguitars738@gmail.com>
2024-04-08 23:16:59 -0300

    Update Ejercicio2.R

--- Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio2.R
+++ Segundo Semestre/Estadística/Unidad 1 Estadistica Descriptiva/Ejercicio2.R
@@ -27,63 +27,12 @@ datos <- data.frame(
 # Imprimir datos
 print(datos)
 
-# Crear el lienzo del gráfico
-par(mfrow = c(1, 2))  # Dividir la ventana gráfica en 1 fila y 2 columnas
-
-# Graficar barras de la cantidad de niños
+# Graficar la frecuencia
 barplot(
-  num_niños,
+  frecuencia,
   names.arg = longitudes,
   col = "skyblue",
   xlab = "Número de metros",
   ylab = "Número de niños",
-  main = "Cantidad de niños por longitud"
+  main = "Frecuencia"
 )
-
-# Graficar las frecuencias y sus acumuladas
-plot(
-  1:length(longitudes),
-  frecuencia,
-  type = "h",
-  lwd = 2,
-  col = "blue",
-  ylim = c(0, max(frecuencia_acumulada) * 1.2),
-  xlab = "Número de metros",
-  ylab = "Frecuencia",
-  main = "Gráfico de frecuencia y frecuencias acumuladas",
-  xaxt = "n"
-)
-axis(side = 1,
-     at = 1:length(longitudes),
-     labels = longitudes)
-
-# Agregar la línea para la frecuencia acumulada
-lines(
-  1:length(longitudes),
-  frecuencia_acumulada,
-  type = "b",
-  pch = 16,
-  col = "red"
-)
-
-# Agregar los puntos para la frecuencia acumulada relativa
-points(
-  1:length(longitudes),
-  frecuencia_acumulada_relativa * max(frecuencia),
-  pch = 19,
-  col = "green"
-)
-
-# Agregar leyenda
-legend(
-  "topright",
-  legend = c(
-    "Frecuencia",
-    "Frecuencia acumulada",
-    "Frecuencia acumulada relativa"
-  ),
-  col = c("blue", "red", "green"),
-  pch = c(15, 16, 19),
-  lty = c(0, 1, 0)
-)
-
