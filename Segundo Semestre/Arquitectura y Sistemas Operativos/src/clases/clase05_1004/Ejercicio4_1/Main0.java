c4c86aca04806481447423bb44f6457ad6400608
Víctor Gomenza <viguitars738@gmail.com>
2024-04-11 18:34:14 -0300

    Create Main0.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio4_1/Main0.java
@@ -0,0 +1,34 @@
+package clases.clase05_1004.Ejercicio4_1;
+
+public class Main0 {
+
+    public static void main(String[] args) {
+
+
+        Runtime runtime = Runtime.getRuntime();
+        int nNucleos = runtime.availableProcessors();
+
+        Thread[] procesos = new Thread[nNucleos];
+
+
+        for (int i = 0; i < procesos.length; i++) {
+
+            Runnable run = new Proceso0();
+            procesos[i] = new Thread(run);
+            procesos[i].start();
+
+        }
+        for (int i = 0; i < procesos.length; i++) {
+
+            try {
+                procesos[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        //System.out.println("Contador "+ Proceso.cont);
+    }
+
+}
