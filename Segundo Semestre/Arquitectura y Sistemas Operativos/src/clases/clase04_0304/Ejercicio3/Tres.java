307567f33df5610919f2064d8018aae4084d9dba
Víctor Gomenza <viguitars738@gmail.com>
2024-04-11 18:34:32 -0300

    Update Tres.java

--- Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase04_0304/Ejercicio3/Tres.java
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase04_0304/Ejercicio3/Tres.java
@@ -6,25 +6,27 @@ public class Tres {
 
         try {
             valor = valor + 1;
-            valor = valor + Integer.parseInt("W");
+            valor = valor + Integer.parseInt("W"); // Aquí se produce una
+            // excepción NumberFormatException
             valor = valor + 1;
             System.out.println("Valor final del try: " + valor);
         } catch (NumberFormatException e) {
-            valor = valor + Integer.parseInt("W");
-
+            valor = valor + Integer.parseInt("W"); // Se vuelve a intentar
+            // convertir "W" a entero, lo que arroja la misma excepción
             System.out.println("Valor final del catch: " + valor);
         } finally {
-            valor = valor + 1;
+            valor = valor + 1; // Se ejecuta sin importar si hay excepciones o no
             System.out.println("Valor final del finally:" + valor);
         }
         valor = valor + 1;
         System.out.println("Valor antes del return: " + valor);
-        return valor;
+        return valor; // Se devuelve el valor 4
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(metodo());
+            System.out.println(metodo()); // Aquí se imprime la excepción
+            // generada por parseInt("W") y luego el valor devuelto por el método
         } catch (Exception e) {
             System.err.println("Excepcion en metodo ( ) ");
             e.printStackTrace();
