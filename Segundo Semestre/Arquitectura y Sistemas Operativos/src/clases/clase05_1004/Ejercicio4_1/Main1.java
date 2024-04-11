0599150f5fff1ee8a706f8c2e75805c348254920
Víctor Gomenza <viguitars738@gmail.com>
2024-04-11 18:34:09 -0300

    Update Main1.java

--- Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio4_1/Main1.java
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio4_1/Main1.java
@@ -1,26 +1,24 @@
-package Ejercicio4_1;
-
-
+package clases.clase05_1004.Ejercicio4_1;
 
 public class Main1 {
 
     public static void main(String[] args) {
 
 
-   Runtime runtime = Runtime.getRuntime();
-   int nNucleos = runtime.availableProcessors();
+        Runtime runtime = Runtime.getRuntime();
+        int nNucleos = runtime.availableProcessors();
 
-    Thread [] procesos = new Thread[nNucleos];
+        Thread[] procesos = new Thread[nNucleos];
 
 
-        for (int i = 0; i < procesos.length ; i++) {
+        for (int i = 0; i < procesos.length; i++) {
 
             Runnable run = new Proceso1(i);
             procesos[i] = new Thread(run);
             procesos[i].start();
 
         }
-        for (int i = 0; i < procesos.length ; i++) {
+        for (int i = 0; i < procesos.length; i++) {
 
             try {
                 procesos[i].join();
