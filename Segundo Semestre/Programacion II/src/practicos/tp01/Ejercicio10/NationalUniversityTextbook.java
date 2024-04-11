758848bf5c471dd74c71e023a97238a34696ef9a
Víctor Gomenza <viguitars738@gmail.com>
2024-04-10 21:12:09 -0300

    Create NationalUniversityTextbook.java

---
+++ Segundo Semestre/Programacion II/src/practicos/tp01/Ejercicio10/NationalUniversityTextbook.java
@@ -0,0 +1,30 @@
+package practicos.tp01.Ejercicio10;
+
+public class NationalUniversityTextbook extends Book {
+
+    //    Atributo
+    private String faculty;
+
+    //    Constructor
+    public NationalUniversityTextbook(String title, String author,
+                                      double price, String faculty) {
+        super(title, author, price);
+        this.faculty = faculty;
+    }
+
+    //    Getters y Setters
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    //  Método específico para imprimir la información del libro
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Faculty: " + faculty);
+    }
+}
