e4c9f29f5477406c286cf3cb1cf22f805402b484
Víctor Gomenza <viguitars738@gmail.com>
2024-04-10 21:12:36 -0300

    Create Circle.java

---
+++ Segundo Semestre/Programacion II/src/practicos/tp01/Ejercicio9/Circle.java
@@ -0,0 +1,33 @@
+package practicos.tp01.Ejercicio9;
+
+public class Circle extends GeometricFigure {
+
+    //    Atributo
+    private double radius;
+
+    //    Constructor
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    //    Getters y Setters
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    //    Método para calcular el área del círculo
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    //    Método para calcular el perímetro del círculo
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
