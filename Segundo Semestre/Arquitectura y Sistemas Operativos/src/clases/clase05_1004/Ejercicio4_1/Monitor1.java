9489af23d449c48223cc2f578d5b71ae772086dc
Víctor Gomenza <viguitars738@gmail.com>
2024-04-11 18:34:52 -0300

    Update Monitor1.java

--- Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio4_1/Monitor1.java
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio4_1/Monitor1.java
@@ -1,30 +1,29 @@
-package Ejercicio4_1;
+package clases.clase05_1004.Ejercicio4_1;
 
 public class Monitor1 {
 
-    private int cont =0;
-    private int order =0;
+    private int cont = 0;
+    private int order = 0;
     private int id;
 
-
-
-    public synchronized  int inc(int parametro){
+    public synchronized int inc(int parametro) {
         for (int i = 0; i < parametro; i++) {
-             cont++;
+            cont++;
         }
         return cont;
     }
-    public synchronized void ordenar(int id ){
 
-        while (id !=order){
+    public synchronized void ordenar(int id) {
+
+        while (id != order) {
             try {
                 wait();// va a una cola diferente de la de synchronized duermen
             } catch (InterruptedException e) {
-               e.printStackTrace();
+                e.printStackTrace();
             }
         }
 
-        System.out.println("Soy el hilo "+id);
+        System.out.println("Soy el hilo " + id);
         order++;
         notifyAll();// pasan de la cola wait al sychronized
 
