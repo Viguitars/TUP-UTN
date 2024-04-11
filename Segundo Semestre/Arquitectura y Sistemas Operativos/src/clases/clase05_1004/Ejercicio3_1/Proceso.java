80fcabd0293870ef141ee114f97046733bf06844
Víctor Gomenza <viguitars738@gmail.com>
2024-04-10 23:34:23 -0300

    Update

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase05_1004/Ejercicio3_1/Proceso.java
@@ -0,0 +1,19 @@
+package Ejercicio3_1;
+
+public class Proceso implements Runnable{
+
+     static int cont =0;
+     private static Object object = new Object();// debe ser estático sino se produce igual el indeterminismo
+
+    @Override
+    public void run() {
+        synchronized (object) { //cual es la diferencia entre poner el synchronized adentro o afuera del bucle
+        for (int i = 0; i <20000 ; i++) {
+           //synchronized (object) {
+               cont++;
+           }
+        }
+        
+
+    }
+}
