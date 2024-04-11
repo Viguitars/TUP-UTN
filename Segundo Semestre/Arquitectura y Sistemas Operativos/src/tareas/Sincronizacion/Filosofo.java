4c0d6f4c3d861a3099bec05f881ab956d546dde9
Víctor Gomenza <viguitars738@gmail.com>
2024-04-11 18:17:39 -0300

    Create Filosofo.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/tareas/Sincronizacion/Filosofo.java
@@ -0,0 +1,44 @@
+package tareas.Sincronizacion;
+
+import java.util.concurrent.Semaphore;
+
+class Filosofo extends Thread {
+    private Semaphore[] palillos;
+    private int id;
+
+    public Filosofo(Semaphore[] palillos, int id) {
+        this.palillos = palillos;
+        this.id = id;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Filósofo " + id + " pensando");
+                Thread.sleep((long) (Math.random() * 5000)); // Pensar
+                System.out.println("Filósofo " + id + " tiene hambre");
+
+                // Intenta tomar el palillo izquierdo
+                palillos[id].acquire();
+                System.out.println("Filósofo " + id + " ha cogido el palillo " + "izquierdo");
+
+                // Intenta tomar el palillo derecho
+                palillos[(id + 1) % palillos.length].acquire();
+                System.out.println("Filósofo " + id + " ha cogido el palillo " + "derecho");
+
+                // Come
+                System.out.println("Filósofo " + id + " comiendo");
+                Thread.sleep((long) (Math.random() * 3000)); // Comer
+
+                // Suelta los palillos
+                palillos[id].release();
+                palillos[(id + 1) % palillos.length].release();
+
+                System.out.println("Filósofo " + id + " ha soltado los " +
+                        "palillos");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
