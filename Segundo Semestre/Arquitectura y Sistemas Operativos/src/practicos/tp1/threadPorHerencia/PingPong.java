c184ceb0c5bd5d2f5a49f884600a16d123d524ac
Víctor Gomenza <viguitars738@gmail.com>
2024-04-13 22:30:21 -0300

    Update PingPong.java

--- Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/threadPorHerencia/PingPong.java
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/practicos/tp1/threadPorHerencia/PingPong.java
@@ -10,17 +10,8 @@ public class PingPong extends Thread {
     }
 
     public void run() { //Se sobrescribe run() de Thread
-        long startTime = System.currentTimeMillis(); // Tiempo de inicio
-
         while (true) {
-            //            System.out.print(word + " ");
             System.out.print(word + " ");
-
-            // Calcular el tiempo transcurrido
-            long elapsedTime = System.currentTimeMillis() - startTime;
-
-            // Imprimir el tiempo transcurrido junto con la palabra
-            System.out.println("(" + elapsedTime + " ms)");
             try {
                 sleep(delay);
             } catch (InterruptedException e) {
