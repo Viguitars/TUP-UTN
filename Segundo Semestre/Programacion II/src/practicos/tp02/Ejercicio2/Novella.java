8f2d438156edd6fa648f4af7b0ca4a57ac6dfa6d
Víctor Gomenza <viguitars738@gmail.com>
2024-04-15 23:09:48 -0300

    Create Novella.java

---
+++ Segundo Semestre/Programacion II/src/practicos/tp02/Ejercicio2/Novella.java
@@ -0,0 +1,19 @@
+package practicos.tp02.Ejercicio2;
+
+public class Novella extends Book {
+    private String genre;
+
+    public Novella(String title, String author, int publicationYear,
+                   String genre) {
+        super(title, author, publicationYear);
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
