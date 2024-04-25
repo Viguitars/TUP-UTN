2a66593bcc7413e8e4f4895fee7246917eadc975
Víctor Gomenza <viguitars738@gmail.com>
2024-04-25 10:45:32 -0300

    Create Main.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase07_2404/EjercicioPeluqueria/Main.java
@@ -0,0 +1,8 @@
+package clases.clase07_2404.EjercicioPeluqueria;
+
+public class Main {
+    public static void main(String[] args) {
+        Peluqueria peluqueria = new Peluqueria();
+        peluqueria.iniciar();
+    }
+}
