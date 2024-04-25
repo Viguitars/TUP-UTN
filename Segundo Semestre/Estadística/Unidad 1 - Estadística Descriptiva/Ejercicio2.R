57951593b7d34f7b1f2d57f5d43615f7cf8ab70f
Víctor Gomenza <viguitars738@gmail.com>
2024-04-25 10:46:44 -0300

    Update Ejercicio2.R

--- Segundo Semestre/Estadística/Unidad 1 - Estadística Descriptiva/Ejercicio2.R
+++ Segundo Semestre/Estadística/Unidad 1 - Estadística Descriptiva/Ejercicio2.R
@@ -1,36 +1,23 @@
 # Datos
-longitudes <- c(1, 2, 3, 4, 5, 6, 7, 8)
-num_niños <- c(2, 10, 10, 5, 10, 3, 2, 2)
+longitud <-
+  c(rep(1, 2),
+    rep(2, 10),
+    rep(3, 10),
+    rep(4, 5),
+    rep(5, 10),
+    rep(6, 3),
+    rep(7, 2),
+    rep(8, 2))
 
-# Frecuencia
-frecuencia <- num_niños
-
-# Frecuencia relativa
-frecuencia_relativa <- num_niños / sum(num_niños)
-
-# Frecuencia acumulada
+# Frecuencias
+frecuencia <- table(longitud)
+frecuencia_relativa <- longitud / length(longitud)
 frecuencia_acumulada <- cumsum(frecuencia)
-
-# Frecuencia acumulada relativa
 frecuencia_acumulada_relativa <- cumsum(frecuencia_relativa)
-
-# Crear un dataframe con los datos
-datos <- data.frame(
-  Longitud = longitudes,
-  Niños = num_niños,
-  f = frecuencia,
-  fr = round(frecuencia_relativa, 3),
-  F = frecuencia_acumulada,
-  Fr = round(frecuencia_acumulada_relativa, 3)
-)
-
-# Imprimir datos
-print(datos)
 
 # Graficar la frecuencia
 barplot(
   frecuencia,
-  names.arg = longitudes,
   col = "skyblue",
   xlab = "Número de metros",
   ylab = "Número de niños",
