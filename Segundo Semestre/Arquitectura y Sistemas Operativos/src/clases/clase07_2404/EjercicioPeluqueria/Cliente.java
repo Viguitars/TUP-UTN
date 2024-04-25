931968ab62728febc26c45f3882edbef12382147
Víctor Gomenza <viguitars738@gmail.com>
2024-04-25 10:45:13 -0300

    Create Cliente.java

---
+++ Segundo Semestre/Arquitectura y Sistemas Operativos/src/clases/clase07_2404/EjercicioPeluqueria/Cliente.java
@@ -0,0 +1,13 @@
+package clases.clase07_2404.EjercicioPeluqueria;
+
+public class Cliente {
+    private final String name;
+
+    public Cliente(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
