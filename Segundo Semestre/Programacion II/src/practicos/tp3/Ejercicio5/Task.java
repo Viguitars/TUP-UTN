dd379ea74c665fea9bca880ca92e01d55fe81bfe
Víctor Gomenza <viguitars738@gmail.com>
2024-04-28 22:19:28 -0300

    Create Task.java

---
+++ Segundo Semestre/Programacion II/src/practicos/tp3/Ejercicio5/Task.java
@@ -0,0 +1,24 @@
+package practicos.tp3.Ejercicio5;
+
+class Task {
+    private String description;
+    private int priority;
+
+    public Task(String description, int priority) {
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Description: " + description + ", Priority: " + priority;
+    }
+}
