3012ab6ca73ddbd0e650acce7a56c70bcb02ee62
Víctor Gomenza <viguitars738@gmail.com>
2024-04-30 20:38:22 -0300

    Create Contract.java

---
+++ Segundo Semestre/Programacion II/src/parciales/primero/Contract.java
@@ -0,0 +1,9 @@
+package parciales.primero;
+
+public interface Contract {
+    double calculateBMI(double weight, double height);
+
+    boolean hasExtraWeight(double weight);
+
+    double takePulse();
+}
