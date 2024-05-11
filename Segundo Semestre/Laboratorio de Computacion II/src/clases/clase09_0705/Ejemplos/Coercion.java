9203f003e7c650dd55073db0d68754ed051b6c60
Víctor Gomenza <viguitars738@gmail.com>
2024-05-11 10:19:33 -0300

    Create Coercion.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Coercion.java
@@ -0,0 +1,15 @@
+package clases.clase09_0705.Ejemplos;
+
+public class Coercion {
+    public static void main(String[] args) {
+        int a = 3;
+        double b = 2.5;
+        /**
+         * Realizamos una operación aritmética entre un entero y un double
+         * La coerción implícita convertirá el entero 'a' en double antes de
+         * la división*/
+        double resultado = a / b;
+        // Imprimimos el resultado
+        System.out.println("El resultado de la división es: " + resultado);
+    }
+}
