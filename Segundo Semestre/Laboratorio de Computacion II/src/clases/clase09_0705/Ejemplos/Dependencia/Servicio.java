22db941557cd316f43f2d22eefc9d70f4dce5af9
Víctor Gomenza <viguitars738@gmail.com>
2024-05-11 10:43:48 -0300

    Create Servicio.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Dependencia/Servicio.java
@@ -0,0 +1,7 @@
+package clases.clase09_0705.Ejemplos.Dependencia;
+
+public class Servicio {
+    public void realizarTarea() {
+        System.out.println("Realizando tarea en el servicio...");
+    }
+}
