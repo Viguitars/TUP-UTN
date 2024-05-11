cff2fdca3f3965f1fe6e2e945eb95d2babd7d713
Víctor Gomenza <viguitars738@gmail.com>
2024-05-11 11:09:03 -0300

    Create Moto.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Herencia/Moto.java
@@ -0,0 +1,8 @@
+package clases.clase09_0705.Ejemplos.Herencia;
+
+public class Moto extends Vehiculo {
+    @Override
+    public void conducir() {
+        System.out.println("Conduciendo la moto...");
+    }
+}
