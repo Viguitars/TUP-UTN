2869e185caea91f89db09260dfbc5f35decd5e2c
Víctor Gomenza <viguitars738@gmail.com>
2024-05-11 11:08:57 -0300

    Create Auto.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Herencia/Auto.java
@@ -0,0 +1,8 @@
+package clases.clase09_0705.Ejemplos.Herencia;
+
+public class Auto extends Vehiculo {
+    @Override
+    public void conducir() {
+        System.out.println("Conduciendo el auto...");
+    }
+}
