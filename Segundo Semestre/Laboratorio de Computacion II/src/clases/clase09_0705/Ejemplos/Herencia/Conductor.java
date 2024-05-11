5a3eb4ba498af9cbd15c8b3667342da129f044a7
Víctor Gomenza <viguitars738@gmail.com>
2024-05-11 12:01:58 -0300

    Update Conductor.java

--- Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Herencia/Conductor.java
+++ Segundo Semestre/Laboratorio de Computacion II/src/clases/clase09_0705/Ejemplos/Herencia/Conductor.java
@@ -1,19 +1,21 @@
 package clases.clase09_0705.Ejemplos.Herencia;
 
 public class Conductor {
-    /**
+    /*
      * La clase Conductor depende de la clase Vehiculo para conducir. La
      * dependencia se establece a través de la composición, donde la clase
-     * Conductor tiene un campo de tipo Vehiculo.**/
+     * Conductor tiene un campo de tipo Vehiculo.
+     */
     private Vehiculo vehiculo;
 
     public Conductor(Vehiculo vehiculo) {
         this.vehiculo = vehiculo;
     }
 
-    /**
+    /*
      * La clase Conductor utiliza el vehículo a través del método
-     * conducirVehiculo(), que llama al método conducir() del objeto vehiculo.**/
+     * conducirVehiculo(), que llama al método conducir() del objeto vehiculo.
+     */
     public void conducirVehiculo() {
         vehiculo.conducir();
     }
