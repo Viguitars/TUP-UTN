af0aa8f0133080bc6d2c6159b9d3959c92dfb601
Víctor Gomenza <viguitars738@gmail.com>
2024-05-13 19:37:11 -0300

    Create Main.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/practicos/tp6_genericas/Ejercicio4/Main.java
@@ -0,0 +1,18 @@
+package practicos.tp6_genericas.Ejercicio4;
+
+import practicos.tp6_genericas.Ejercicio3.Producto;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear una instancia de Almacen
+        Almacen<Producto> almacen = new Almacen<>();
+
+        // Agregar varios productos de diferentes tipos
+        almacen.agregarElemento(new Producto("Leche", 2.5));
+        almacen.agregarElemento(new Producto("Pan", 1.0));
+        almacen.agregarElemento(new Producto("Televisor", 500.0));
+
+        // Imprimir los productos almacenados
+        almacen.imprimirElementos();
+    }
+}
