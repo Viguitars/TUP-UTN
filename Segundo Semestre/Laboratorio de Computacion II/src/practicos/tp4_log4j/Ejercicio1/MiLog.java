35ce3c07d9e2a2c9b34209c8be264f4c466479fd
Víctor Gomenza <viguitars738@gmail.com>
2024-05-13 19:12:55 -0300

    Create MiLog.java

---
+++ Segundo Semestre/Laboratorio de Computacion II/src/practicos/tp4_log4j/Ejercicio1/MiLog.java
@@ -0,0 +1,19 @@
+// package practicos.tp4.Ejercicio1;
+//
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+//
+// public class MiLog {
+//     // Obtenemos el logger para esta clase
+//     private static final Logger logger = LogManager.getLogger(MiLog.class);
+//
+//     public static void main(String[] args) {
+//         // Mensaje de inicio de la aplicación
+//         logger.info("La aplicación MiLog se ha iniciado.");
+//
+//         // Tu código aquí...
+//
+//         // Mensaje de finalización de la aplicación
+//         logger.info("La aplicación MiLog ha finalizado.");
+//     }
+// }
